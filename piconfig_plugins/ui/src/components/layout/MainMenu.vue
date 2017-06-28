53d2d52c06bbf81abc78ddaccfce4fd92e061d1e
Ben McClure <ben.mcclure@gmail.com>
2017-06-28 02:30:32 -0500

    Turn on ripple effect on main menu

--- piconfig_plugins/ui/src/components/layout/MainMenu.vue
+++ piconfig_plugins/ui/src/components/layout/MainMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <v-list>
     <v-list-group v-for="item in items" :value="item.active" v-bind:key="item.title">
-      <v-list-tile slot="item" :router="true" :href="item.route && !item.items ? item.route : null">
+      <v-list-tile slot="item" :router="true" :href="item.route && !item.items ? item.route : null" ripple>
         <v-list-tile-action>
           <v-icon>{{ item.action }}</v-icon>
         </v-list-tile-action>
@@ -13,7 +13,7 @@
         </v-list-tile-action>
       </v-list-tile>
       <v-list-item v-for="subItem in item.items" v-bind:key="subItem.title">
-        <v-list-tile :router="true" :href="subItem.route ? subItem.route : null">
+        <v-list-tile :router="true" :href="subItem.route ? subItem.route : null" ripple>
           <v-list-tile-content>
             <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
           </v-list-tile-content>
